a8588aff98075cd001f522b7f7d3176f8e4cd843
KJ-30 <1810459894@qq.com>
2024-11-07 16:16:09 +0800

    解决冲突

--- src/routes/handlers/home_handler.rs
+++ src/routes/handlers/home_handler.rs
@@ -4,7 +4,6 @@ use sea_orm::{ConnectionTrait, Statement};
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> impl Responder {
     api_response::ApiResponse::new(200, format!("Hello {}!", name))
-    api_response::ApiResponse::new(200, format!("Hello {}!", name))
 }
 #[get("/test")]
 pub async fn test(api_state: web::Data<AppState>) -> impl Responder {
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,6 +1,5 @@
 use lazy_static::lazy_static;
 use std::env;
-use std::env;
 lazy_static! {
     pub static ref ADDRESS: String = set_address();
     pub static ref PORT: u16 = set_port();
