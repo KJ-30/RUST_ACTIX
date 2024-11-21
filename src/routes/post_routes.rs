e77f0f162ac63b8705469a6d0dfbbf4a9b5d5373
KJ-30 <1810459894@qq.com>
2024-11-21 11:20:28 +0800

    接口路由配置文件

--- src/routes/post_routes.rs
+++ src/routes/post_routes.rs
@@ -4,7 +4,12 @@ use actix_web_lab::middleware::from_fn;
 pub fn config(config: &mut web::ServiceConfig) {
     config
         .service(
-            web::scope("/post").wrap(from_fn(middlewares::auth_middleware::check_auth_middleware)),
+            web::scope("/post")
+                .wrap(from_fn(middlewares::auth_middleware::check_auth_middleware))
+                .service(handlers::post_handler::create_post).service(handlers::post_handler::get_my_post),
         )
-        .service(web::scope("/post"));
+        .service(
+            web::scope("/post")
+            .service(handlers::post_handler::get_all_post).service(handlers::post_handler::get_one_post)
+        );
 }
